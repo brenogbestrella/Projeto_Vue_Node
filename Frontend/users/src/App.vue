bb86a5c3a57daef278cf9c80f24d7aaf2b9c0d4b
Breno Estrella <brenogbestrella@gmail.com>
2021-09-27 22:36:47 -0300

    Frontend com Vue.js e Bootstrap
    Criação da SPA com Vue.js utilizando Bootstrap para exibir os dados da API do Node.js

---
+++ Frontend/users/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <b-container>
+      <b-row>
+        <b-table bordered dark striped hover :items="posts" :fields="fields">
+        </b-table>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import api from '@/service/api.js'
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      fields: ['id', 'nome', 'age'],
+      items: [],
+      posts: []
+    }
+  },
+  mounted () {
+    api.get('/users').then(response => {
+      console.log(response)
+      this.posts = response.data
+    })
+  }
+}
+</script>
